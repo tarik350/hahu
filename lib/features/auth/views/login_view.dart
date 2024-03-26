62e49c9c686d44ee67b0e4c374b4dbb4e62b35dc
tarik350 <80385670+tarik350@users.noreply.github.com>
2024-03-27 00:10:36 +0300

    first commit

---
+++ lib/features/auth/views/login_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class LoginView extends StatelessWidget {
+  const LoginView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/features/auth/views/signup_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class SignupView extends StatelessWidget {
+  const SignupView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
